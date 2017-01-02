6dfe21ae0e86a896904459d80cde6e1e2c903b25
Mukesh Kumar <singh88rajat@gmail.com>
2017-01-02 23:42:31 +0530

    Boolean Test

---
+++ src/ThirdSession2ndAssignment.java
@@ -0,0 +1,18 @@
+
+public class ThirdSession2ndAssignment {
+	public static void main(String[] args) {
+		String s1 = "blue";
+		String s2 = "blue";
+		// Use expression to compute a boolean value.
+		boolean same = s1==s2;
+		// Test our boolean.
+		if (same) {
+		    System.out.println("S1 and S2 are Same = " + same);
+		}
+		else{
+			System.out.println("S1 and S2 are Same = " + same);
+			
+		}
+	}
+
+}
